ccf403ef4e4a6742db112cfd7311a2245feb7a51
slpboy <binancesenura@gmail.com>
2025-05-17 01:53:47 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,43 @@
+/**
+
+
+ Copyright (C) 2025.
+ Licensed under the  GPL-3.0 License;
+ You may not sell this script.
+ It is supplied in the hope that it may be useful.
+ * @project_name : Free Bot script
+ * @author : Malvin King <https://github.com/kingmalvn>
+ * @description : A Multi-functional whatsapp bot script.
+ * @version 3.0.0
+ **/
+
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "private",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.fm/u/hpbhm37259",
+MENU_IMG: process.env.MENU_IMG || "https://files.fm/u/hpbhm37259",
+ALIVE_MSG: process.env.ALIVE_MSG || "✅🍭 *🫟SENURA LAKSHAN🫟IS ONLINE NOW AND +500 MORE FEATURES AVAILABLE NOW THIS BOT*",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+FAKE_TYPING: process.env.FAKE_TYPING || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+HEART_REACT: process.env.HEART_REACT || "false",
+OWNER_REACT: process.env.OWNER_REACT || "false",
+BOT_NAME: process.env.BOT_NAME || "『SENU-MD』",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
